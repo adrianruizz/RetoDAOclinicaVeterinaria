a94dbf3348eefc9e7fa4c3416c458e3663fd6cfb
1DAM <1DAM@A11-PC22>
2026-05-15 09:34:28 +0100

    57

--- RetoDAOclinicaVeterinaria/src/app/Main.java
+++ RetoDAOclinicaVeterinaria/src/app/Main.java
@@ -9,19 +9,8 @@ public class Main {
 
 	public static void main(String[] args) {
 	
-		Scanner scanner = new Scanner(System.in);
-ClienteDAO c23 = new ClienteDAO();
-		
-		for (Cliente c233 : c23.obtenerTodos()) {
-			
-			System.out.println(c233);
-	
-		}
-		System.out.print("Introduce tu id: ");
-	       int id =Integer.parseInt(scanner.nextLine()) ;
-	      
-	      
-	       System.out.println(c23.obtenerPorId(id));
+
+	       
 
 	}
 }
