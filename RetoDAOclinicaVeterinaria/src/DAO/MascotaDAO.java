2474d2fc5001470861a0e57ec90f278ea863aab4
ChinCobre <jandro.github@gmail.com>
2026-05-15 09:38:15 +0200

    tratamiento y mascosta

--- RetoDAOclinicaVeterinaria/src/DAO/MascotaDAO.java
+++ RetoDAOclinicaVeterinaria/src/DAO/MascotaDAO.java
@@ -1,5 +1,77 @@
 package DAO;
 
-public class MascotaDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
+import Modulo.Cliente;
+import Modulo.Mascota;
+import Modulo.Tratamiento;
+import Util.ConexionBD;
+
+public class MascotaDAO implements GenericDAO<Mascota>{
+
+	@Override
+	public boolean insertar(Mascota objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Mascota> obtenerTodos() {
+		ArrayList<Mascota> lista = new ArrayList<>();
+		String sql = "select id_mascota, id_cliente, nombre, especie, fecha_nacimiento, peso from mascotas ";
+		try (Connection con = ConexionBD.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+
+			
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				lista.add(mapear(rs));
+			}
+		} catch (SQLException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		return lista;
+
+	}
+	
+
+	@Override
+	public Mascota obtenerPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean actualizar(Mascota objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eliminar(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+
+	private Mascota mapear(ResultSet rs) throws SQLException {
+		Mascota a = new Mascota();
+		a.setId_mascota(rs.getInt("id_mascota"));
+		a.setId_cliente(rs.getInt("id_cliente"));
+		a.setNombre(rs.getString("nombre"));
+		a.setEspecie(rs.getString("especie"));
+		a.setPeso(rs.getDouble("peso"));
+		a.setFecha_nacimiento(rs.getObject("fecha_nacimiento", LocalDate.class));
+		return a;
+	}
+
+	
+	
 }
--- RetoDAOclinicaVeterinaria/src/DAO/TratamientoDAO.java
+++ RetoDAOclinicaVeterinaria/src/DAO/TratamientoDAO.java
@@ -1,5 +1,93 @@
 package DAO;
 
-public class TratamientoDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import Modulo.Cliente;
+import Modulo.Tratamiento;
+import Util.ConexionBD;
+
+public class TratamientoDAO implements GenericDAO<Tratamiento> {
+
+	@Override
+	public boolean insertar(Tratamiento objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<Tratamiento> obtenerTodos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Tratamiento obtenerPorId(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean actualizar(Tratamiento objeto) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean eliminar(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public ArrayList<Tratamiento> obtenerTratamientos(int id_veterinario) {
+		ArrayList<Tratamiento> lista = new ArrayList<>();
+		String sql = " select t.id_tratamiento as id_tratamiento, nombre, precio from tratamientos t inner join historial h on t.id_tratamiento=h.id_tratamiento inner join veterinarios v on v.id_veterinario=h.id_veterinario where v.id_veterinario=?";
+
+		try (Connection con = ConexionBD.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+
+			ps.setInt(1, id_veterinario);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				lista.add(mapear(rs));
+			}
+		} catch (SQLException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		return lista;
+
+	}
+
+	public ArrayList<Tratamiento> obtenerTratamientosPorIdMascota(int id_mascota) {
+		ArrayList<Tratamiento> lista = new ArrayList<>();
+		String sql = " select t.id_tratamiento as id_tratamiento, nombre, precio from tratamientos t inner join historial h on t.id_tratamiento=h.id_tratamiento inner join veterinarios v on v.id_veterinario=h.id_veterinario where v.id_veterinario=?";
+
+		try (Connection con = ConexionBD.getConnection(); PreparedStatement ps = con.prepareStatement(sql)) {
+
+			ps.setInt(1, id_mascota);
+			ResultSet rs = ps.executeQuery();
+			while (rs.next()) {
+				lista.add(mapear(rs));
+			}
+		} catch (SQLException e) {
+			System.out.println("Error: " + e.getMessage());
+		}
+
+		return lista;
+
+	}
+	private Tratamiento mapear(ResultSet rs) throws SQLException {
+		Tratamiento a = new Tratamiento();
+		a.setId_tratamiento(rs.getInt("id_tratamiento"));
+		a.setNombre(rs.getString("nombre"));
+		a.setPrecio(rs.getDouble("precio"));
+		return a;
+	}
+
+	
 
 }
